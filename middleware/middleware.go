a866dce703f2259cf1a006f83d7761e7f96f0639
German Lena <german.lena@gmail.com>
2015-12-30 23:46:56 -0300

    auth0 user linking added unsecure middleware to load the user if token is provided

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,16 +5,36 @@ import (
 	"app/middleware/users"
 
 	"github.com/labstack/echo"
+	"net/http"
 )
 
 var authM *auth.JWTMiddleware
 var usersM *users.UsersMiddleware
 
-func middlewareAuth(c *echo.Context) error {
+func middlewareAuth(c *echo.Context, forceLoggedUser bool) error {
+
 	err := authM.Handler()(c)
 	if err != nil {
-		return err
+		if forceLoggedUser {
+			return err
+		}
+		return nil
 	}
+
+	if forceLoggedUser {
+		he := echo.NewHTTPError(http.StatusUnauthorized)
+
+		if c.Get("claims") == nil {
+			return he
+		}
+
+		claims := c.Get("claims").(map[string]interface{})
+
+		if _, ok := claims["sub"]; !ok {
+			return he
+		}
+	}
+
 	err = usersM.Handler()(c)
 	return err
 }
@@ -25,12 +45,17 @@ func ConfigRestrictMiddleware(signingKey []byte, aud string) {
 }
 
 func Secure(delegate echo.HandlerFunc) echo.HandlerFunc {
+	return middlewareWrapper(delegate, true)
+}
+func Unsecure(delegate echo.HandlerFunc) echo.HandlerFunc {
+	return middlewareWrapper(delegate, false)
+}
+func middlewareWrapper(delegate echo.HandlerFunc, forceLoggedUser bool) echo.HandlerFunc {
 	return func(c *echo.Context) error {
-		if err := middlewareAuth(c); err != nil {
+		if err := middlewareAuth(c, forceLoggedUser); err != nil {
 			return err
 		} else {
 			return delegate(c)
 		}
 	}
-
 }
--- middleware/users/users.go
+++ middleware/users/users.go
@@ -1,44 +1,83 @@
 package users
 
 import (
-  "app/models"
-  "github.com/labstack/echo"
+	"app/models"
+	"fmt"
+	"github.com/labstack/echo"
+	"strings"
 )
 
-
 func NewUsersMiddleware() *UsersMiddleware {
-  return &UsersMiddleware{}
+	return &UsersMiddleware{}
 }
 
 type UsersMiddleware struct {
 }
 
-func (me *UsersMiddleware) Handler() echo.HandlerFunc { 
+func (me *UsersMiddleware) Handler() echo.HandlerFunc {
 
-  return func(c *echo.Context) error {
-    
-    // Skip WebSocket
-    if (c.Request().Header.Get(echo.Upgrade)) == echo.WebSocket {
-      return nil
-    }
+	return func(c *echo.Context) error {
 
-    claims := c.Get("claims").(map[string]interface{})
-    sub := claims["sub"].(string)
+		// Skip WebSocket
+		if (c.Request().Header.Get(echo.Upgrade)) == echo.WebSocket {
+			return nil
+		}
 
-    user := models.User{}
+		claims := c.Get("claims").(map[string]interface{})
 
-    if models.DB.Where(&models.User{Auth0Id: sub}).First(&user).RecordNotFound() {
+		sub := getClaim(claims, "sub")
 
-      models.DB.Create(&models.User{
-        Auth0Id: sub,
-      })
+		if sub == "" {
+			return nil
+		}
 
-    }
+		name := getClaim(claims, "first_name")
+		family_name := getClaim(claims, "family_name")
+		email := getClaim(claims, "email")
+		avatar := getClaim(claims, "avatar")
 
-    c.Set("User", user)
+		user := models.User{}
 
-    return nil
+		// Check if the user exists with the auth0 user_id
+		if models.DB.Where(&models.User{Auth0Id: sub}).First(&user).RecordNotFound() {
 
-  }
+			id_parts := strings.SplitN(sub, "|", 2)
 
+			provider := id_parts[0]
+			provider_id := id_parts[1]
+
+			// check if legacy user exists
+			if models.DB.Where(&models.User{Provider: provider, ProviderId: provider_id}).First(&user).RecordNotFound() {
+
+				// it does not exists, lets create a new one
+				models.DB.Create(&models.User{
+					Auth0Id:    sub,
+					Name:       name + " " + family_name,
+					Email:      email,
+					Avatar:     avatar,
+					Provider:   provider,
+					ProviderId: provider_id,
+				})
+
+			} else {
+				// it exists so lets link to the auth0 user_id
+				user.Auth0Id = sub
+				models.DB.Model(&user).Updates(user)
+			}
+
+		}
+
+		c.Set("User", user)
+
+		return nil
+
+	}
+
+}
+
+func getClaim(claims map[string]interface{}, key string) string {
+	if claims[key] == nil {
+		return ""
+	}
+	return claims[key].(string)
 }
